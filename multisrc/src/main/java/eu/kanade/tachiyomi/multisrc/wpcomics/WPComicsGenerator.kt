1db8d4d4b46fc2899d72d98f79fabc14e97cac16
Mia <134089159+MiaLui@users.noreply.github.com>
2024-02-24 18:26:49 +0700

    update new

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/wpcomics/WPComicsGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/wpcomics/WPComicsGenerator.kt
@@ -12,10 +12,7 @@ class WPComicsGenerator : ThemeSourceGenerator {
     override val baseVersionCode: Int = 2
 
     override val sources = listOf(
-        SingleLang("NetTruyen", "https://www.nettruyenss.com/", "vi", overrideVersionCode = 20),
-        SingleLang("NhatTruyen", "https://nhattruyenmax.com", "vi", overrideVersionCode = 13),
-        SingleLang("TruyenChon", "http://truyenchon.com", "vi", overrideVersionCode = 3),
-        SingleLang("XOXO Comics", "https://xoxocomics.com", "en", className = "XoxoComics", overrideVersionCode = 2),
+        SingleLang("NetTruyen", "https://www.nettruyenbb.com/", "vi", overrideVersionCode = 20)
     )
 
     companion object {
