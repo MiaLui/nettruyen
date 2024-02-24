9469c40b2a77e1aa1304ae8d457c35cee775fdd4
Mia <134089159+MiaLui@users.noreply.github.com>
2024-02-24 18:14:45 +0700

    update

---
+++ core/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    compileSdk = AndroidConfig.compileSdk
+
+    defaultConfig {
+        minSdk = AndroidConfig.minSdk
+    }
+
+    namespace = "eu.kanade.tachiyomi.revived"
+
+    @Suppress("UnstableApiUsage")
+    sourceSets {
+        named("main") {
+            manifest.srcFile("AndroidManifest.xml")
+            res.setSrcDirs(listOf("res"))
+        }
+    }
+
+    libraryVariants.all {
+        generateBuildConfigProvider?.configure {
+            enabled = false
+        }
+    }
+}
